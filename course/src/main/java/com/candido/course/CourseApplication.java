d85e487e36d3b5ff96a285c5b591709bff3f256c
Douglas Candido <doug.candido2704@gmail.com>
2024-05-08 04:57:17 -0300

    Service layer, component registration

--- course/src/main/java/com/candido/course/CourseApplication.java
+++ course/src/main/java/com/candido/course/CourseApplication.java
@@ -2,12 +2,14 @@ package com.candido.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.candido") //alteração do chatgpt
 public class CourseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CourseApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(CourseApplication.class, args);
+    }
 
 }
